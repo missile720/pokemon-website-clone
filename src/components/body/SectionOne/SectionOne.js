6680eaf138d81f70466fda580769568b73d19012
Misael Bolainez <misaelbolainez@gmail.com>
2022-06-30 16:39:06 -0700

    feat(sticky Nav): Added the fixed Nav when it gets to the top of its container

--- src/components/body/SectionOne/SectionOne.js
+++ src/components/body/SectionOne/SectionOne.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Feature from './Feature'
 import RowOne from './RowOne'
 import RowTwo from './RowTwo'
@@ -6,8 +6,28 @@ import SideNav from './SideNav'
 import './SectionOne.css'
 
 function SectionOne() {
+  const[offset,setOffset] = useState('');
+
+  useEffect(() => {
+    const handleScroll = event => {
+      console.log('window.scrollY', window.scrollY);
+      if (window.scrollY >= 68) {
+        setOffset("offset");
+      } 
+      else {
+        setOffset("");
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return() => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className='sectionOneContainer'>
+    <div className={'sectionOneContainer ' + (offset)}>
       <SideNav/>
       <RowOne/>
       <RowTwo/>
